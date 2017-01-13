a3e22d519bc14f9a7380dc60430302b2b4626834
Ivan Stepić <stepicivan@gmail.com>
2017-01-13 10:27:53 +0100

    change build folder, embed html and css in JS

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,28 +1,24 @@
 let gulp = require('gulp'),
-sass = require('gulp-sass'),
-runSequence = require('run-sequence'),
-minifyCss = require('gulp-minify-css'),
-sourcemaps = require('gulp-sourcemaps'),
-tsc = require('gulp-typescript'),
-embedTemplates = require('gulp-inline-ng2-template'),
-clean = require('gulp-clean');
+    sass = require('gulp-sass'),
+    runSequence = require('run-sequence'),
+    minifyCss = require('gulp-minify-css'),
+    sourcemaps = require('gulp-sourcemaps'),
+    tsc = require('gulp-typescript'),
+    embedTemplates = require('gulp-inline-ng2-template'),
+    clean = require('gulp-clean');
 
 //----
 //build steps
 gulp.task('build', function (done) {
-    runSequence('clean', 'compile-sass', 'copy-html', 'compile-typings');
+    runSequence('clean', 'compile-typings');
 });
 //----
 //clearing the output dir
 gulp.task('clean', function (done) {
-    return gulp.src('dist', {read: false})
+    return gulp.src('lib', { read: false })
         .pipe(clean());
 });
-
-gulp.task('copy-html', function () {
-    return gulp.src('src/*.html')
-        .pipe(gulp.dest("dist/src"));
-});
+ 
 //----
 //Sass compilation and minifiction
 gulp.task('compile-sass', function () {
@@ -30,8 +26,8 @@ gulp.task('compile-sass', function () {
         .pipe(sass().on('error', sass.logError)) // this will prevent our future watch-task from crashing on sass-errors
         .pipe(minifyCss({ compatibility: 'ie8' })) // see the gulp-sass doc for more information on compatibilitymodes
         .pipe(gulp.dest(function (file) {
-        return file.base; // because of Angular 2's encapsulation, it's natural to save the css where the scss-file was
-    }));
+            return file.base; // because of Angular 2's encapsulation, it's natural to save the css where the scss-file was
+        }));
 });
 //----
 //typescript compilation including sourcemaps and template embedding
@@ -39,8 +35,11 @@ gulp.task('compile-typings', function () {
     //loading typings file
     let tsProject = tsc.createProject('tsconfig.json');
     return tsProject.src()
+        .pipe(embedTemplates({
+            base: '/src'
+        }))
         .pipe(tsProject())
         .pipe(sourcemaps.init())
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('lib'));
 });
